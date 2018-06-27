e909ab6dc0257e546bd8d5448a9d24799a707515
tpettrov <tpettrov@gmail.com>
2018-06-27 13:54:59 +0300

    dist

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
 const distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(distDir, 'index.html'));
+});
+
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
